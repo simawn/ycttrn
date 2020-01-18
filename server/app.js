f3ffbc68274c6faefa4a72f7990efb27dd1da51f
SunXP <roger_lu_dude@hotmail.com>
2020-01-18 16:48:34 -0500

    Change url params to query parameters

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const turf = require('@turf/turf');
 const app = express();
 
 const data = require('./db.json');
 
+filterByRadius = (lat, lon, r) => {
+    data.forEach(element => {
+        const from = turf.point([lat, lon]);
+        const to = turf.point([])
+        console.log(element.latitude);
+    });
+}
 
-
-app.get('/:coords', (req, res) => {
+app.get('/coords', (req, res) => {
+    const queryParam = req.query;
+    const latitude = queryParam.lat;
+    const longitude = queryParam.lon;
+    const radius = queryParam.r;
     res.header("Content-Type",'application/json');
+    //filterByRadius(latitude, longitude, radius);
     res.send(JSON.stringify(data));
 });
 
