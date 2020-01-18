5a035fe0960f25c7634da433bf2004cff337490b
Simon Huang <simon@simonhuang.ca>
2020-01-18 18:36:26 -0500

    Add temp parameters to fetch URL

--- client/src/components/APIFetch.js
+++ client/src/components/APIFetch.js
@@ -2,7 +2,7 @@ import { BACKEND_IP } from "./APISettings";
 
 export let getNearbyPoints = async (lat, lon, radius) => {
   try {
-    let path = "http://127.0.0.1:4000/coords";
+    let path = "http://127.0.0.1:4000/coords?lat=45.5017&lon=73.5673&r=100";
     let response = await fetch(path, {
         headers: { "Content-Type": "application/json", },
     });
