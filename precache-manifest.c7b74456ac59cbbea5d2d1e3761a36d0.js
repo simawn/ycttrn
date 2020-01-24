ad5f1bfb372e0e2b947e686decb68dc50cb2bf32
Simon Huang <simon@simonhuang.ca>
2020-01-23 21:44:49 -0500

    Updates

---
+++ precache-manifest.c7b74456ac59cbbea5d2d1e3761a36d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaad3b97df4d839a6380c831df0a4d7",
+    "url": "/ycttrn/index.html"
+  },
+  {
+    "revision": "de2ea2bd8690153f19eb",
+    "url": "/ycttrn/static/css/2.61091e02.chunk.css"
+  },
+  {
+    "revision": "d322c6057a373fe8c01f",
+    "url": "/ycttrn/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "de2ea2bd8690153f19eb",
+    "url": "/ycttrn/static/js/2.7ec5b641.chunk.js"
+  },
+  {
+    "revision": "cbf4f58793ac3d0e69dbc5e76e6b36ed",
+    "url": "/ycttrn/static/js/2.7ec5b641.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d322c6057a373fe8c01f",
+    "url": "/ycttrn/static/js/main.03e206cb.chunk.js"
+  },
+  {
+    "revision": "5e67382885f5d83b3b0a",
+    "url": "/ycttrn/static/js/runtime-main.f3f33ac5.js"
+  },
+  {
+    "revision": "8a69fb461e3ae83bef1d4d1a2c43326a",
+    "url": "/ycttrn/static/media/ycttrn-logo-400-crop.8a69fb46.png"
+  }
+]);
